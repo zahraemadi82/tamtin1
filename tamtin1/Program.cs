b55c313b1d063943b93e5afe9ea3b06793a6aef2
User <User@DESKTOP-1364VBG>
2024-01-29 17:41:05 +0330

    Add project files.

---
+++ tamtin1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace project
+{
+    class Test
+    {
+        public static void Main()
+        {
+            double w = Convert.ToDouble(Console.ReadLine());
+            double l = Convert.ToDouble(Console.ReadLine());
+            double h = Convert.ToDouble(Console.ReadLine());
+            double result = name(l, h, w);
+            {
+                Console.WriteLine(result);
+            }
+
+
+            static double name(double l, double h, double w)
+            {
+                double voloum = l * h * w;
+                return voloum;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
